317886c7b3451f31fc3c55f33aeab28334b9770c
Gregg Trunnell <gggtrunnell@gmail.com>
2024-11-16 21:10:11 -0600

    addItem POST populates db

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,13 +1,10 @@
-console.log('JS is sourced!');
 
-
-
+//
 function addItem(event){
     event.preventDefault();
     console.log("POST in client")
-    
         const newItem={
-            text: document.getElementById('todoItem').value
+            text: document.getElementById('toDoItem').value
         };
         axios({
             method: 'POST',
@@ -52,6 +49,31 @@ function deleteItem( id ){
           console.log('Got items from GET in client', response.data);
           renderItems();
         }).catch((error)=> {
-          console.log('error in song get', error);
+          console.log('error in GET client', error);
         });
     }
+
+    function toggleIsComplete( id, readyToComplete){
+        console.log("ready to complete PUT in client")
+        const itemToComplete = {
+          id: id,
+          completed: true
+        };
+        if( readyToComplete ){
+            itemToComplete.completed = false;
+        }
+      // Send the new artist to the server as data
+        axios({
+          method: 'PUT',
+          url: '/todos',
+          data: itemToComplete
+        }).then((response) =>{
+          console.log("item in PUT client", response.data);
+          itemsList();
+        }).catch(function(error) {
+          console.log('error in PUT client', error); 
+          alert('These items do not want to be updated')
+        });
+      }
+
+
